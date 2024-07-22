e715afab4441ee386a7c250e4254121116cf0cf9
Godwin Idemudia <godwinide@gmail.com>
2024-07-22 12:37:14 +0100

    new

--- routes/user.js
+++ routes/user.js
@@ -79,10 +79,10 @@ router.post("/withdraw", ensureAuthenticated, async (req, res) => {
             return res.redirect("/withdraw");
         }
         else {
-            // await User.updateOne({ _id: req.user.id }, {
-            //     pending_withdrawal: Number(req.user.pending_withdrawal || 0) + Number(realamount),
-            //     // balance: Number(req.user.balance) - Number(realamount)
-            // })
+            await User.updateOne({ _id: req.user.id }, {
+                pending_withdrawal: Number(req.user.pending_withdrawal || 0) + Number(realamount),
+                balance: Number(req.user.balance) - Number(realamount)
+            });
             req.flash("success_msg", `Your current pending approval, contact support for assistance`);
             return res.redirect("/withdraw");
         }
